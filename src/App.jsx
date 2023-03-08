1e7828b7939b813feec397362158802530fbd0fb
veronicamayer <mayerveronica.vm@gmail.com>
2023-03-08 13:47:16 +0000

    created all files for pages and components, renamed all files from .js to .jsx and .css to .scss

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.scss';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/home/Home'
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;

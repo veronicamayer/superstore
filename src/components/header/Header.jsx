c2dab3fe0245c4c023dff340edb1c08dbf916fba
veronicamayer <mayerveronica.vm@gmail.com>
2023-03-09 15:28:11 +0000

    fixed small bugs for netlify

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import filterMenueImg from "../../images/filterMenu.png"
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import "./Header.scss"
 

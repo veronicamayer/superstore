61add013f185529df1d42c3a25771a04243b27be
veronicamayer <mayerveronica.vm@gmail.com>
2023-03-09 12:09:32 +0000

    added Category component and fetch to Home

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,7 +1,102 @@
+import { v4 as uuidv4 } from "uuid";
+import './Category.scss';
+
+/* images import */
+import smartphones from "../../images/smartphones.png";
+import laptops from "../../images/laptops.png";
+import fragrances from "../../images/fragrances.png";
+import skincare from "../../images/skincare.png";
+import groceries from "../../images/groceries.png";
+import homeDecoration from "../../images/fragrances.png";
+import furniture from "../../images/furniture.png";
+import tops from "../../images/tops.png";
+import womensDresses from "../../images/womens-dresses.png";
+import womensShoes from "../../images/womens-shoes.png";
+import mensShirts from "../../images/mens-shirts.png";
+import mensShoes from "../../images/mens-shoes.png";
+import mensWatches from "../../images/mens-watches.png";
+import womensWatches from "../../images/womens-watches.png";
+import womensBags from "../../images/womens-bags.png";
+import womensJewellery from "../../images/womens-jewellery.png";
+import sunglasses from "../../images/sunglasses.png";
+import automotive from "../../images/automotive.png";
+import motorcycle from "../../images/motorcycle.png";
+import lighting from "../../images/lighting.png";
+
 const Category = (props) => {
-    return ( 
-        <h2>Category</h2>
-     );
-}
- 
+    let imageSrc;
+    switch (props.category) {
+        case "smartphones":
+            imageSrc = smartphones;
+            break;
+        case "laptops":
+            imageSrc = laptops;
+            break;
+        case "fragrances":
+            imageSrc = fragrances;
+            break;
+        case "skincare":
+            imageSrc = skincare;
+            break;
+        case "groceries":
+            imageSrc = groceries;
+            break;
+        case "home-decoration":
+            imageSrc = homeDecoration;
+            break;
+        case "furniture":
+            imageSrc = furniture;
+            break;
+        case "tops":
+            imageSrc = tops;
+            break;
+        case "womens-dresses":
+            imageSrc = womensDresses;
+            break;
+        case "womens-shoes":
+            imageSrc = womensShoes;
+            break;
+        case "mens-shirts":
+            imageSrc = mensShirts;
+            break;
+        case "mens-shoes":
+            imageSrc = mensShoes;
+            break;
+        case "mens-watches":
+            imageSrc = mensWatches;
+            break;
+        case "womens-watches":
+            imageSrc = womensWatches;
+            break;
+        case "womens-bags":
+            imageSrc = womensBags;
+            break;
+        case "womens-jewellery":
+            imageSrc = womensJewellery;
+            break;
+        case "sunglasses":
+            imageSrc = sunglasses;
+            break;
+        case "automotive":
+            imageSrc = automotive;
+            break;
+        case "motorcycle":
+            imageSrc = motorcycle;
+            break;
+        case "lighting":
+            imageSrc = lighting;
+            break;
+        default:
+            imageSrc = null;
+            break;
+    }
+
+    return (
+        <article key={uuidv4()} id="category">
+            <img src={imageSrc} alt="" />
+            <p>{props.category}</p>
+        </article>
+    );
+};
+
 export default Category;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,34 +1,41 @@
-import Header from "../../components/header/Header"
-import Category from "../../components/category/Category"
-import ProductCard from "../../components/productCard/ProductCard"
-import Footer from "../../components/footer/Footer"
-import { useState } from "react"
+import Header from "../../components/header/Header";
+import Category from "../../components/category/Category";
+import ProductCard from "../../components/productCard/ProductCard";
+import Footer from "../../components/footer/Footer";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  const [products, setProducts] = useState([])
-  const [categories, setCategories] = useState([])
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-  return (
-      <section>
-        <h2>
-          Find your favorite Product
-        </h2>
-        <Header/>
-        {categories && categories.map(category => {return (
-          <Category/>
-        )})}
-        <div>
-          <h2>Popular</h2>
-          <a href="">View all</a>
-        </div>
-        {products && products.map(product => {return (
-          <ProductCard/>
-        )})}
-        <Footer/>
+    useEffect(() => {
+        fetch("https://dummyjson.com/products/categories")
+            .then((response) => response.json())
+            .then((data) => setCategories(data))
+            .catch((error) => console.error(error));
+    }, []);
 
-      </section>
-
-  )
+    return (
+        <section>
+            <h2>Find your favorite Product</h2>
+            <Header />
+            <article id="allCategories">
+                {categories &&
+                    categories.map((category) => {
+                        return <Category category={category} />;
+                    })}
+            </article>
+            <div>
+                <h2>Popular</h2>
+                <a href="">View all</a>
+            </div>
+            {products &&
+                products.map((product) => {
+                    return <ProductCard />;
+                })}
+            <Footer />
+        </section>
+    );
 };
 
 export default Home;

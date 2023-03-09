e187904aab86740648ff65dc0e83d58116c72bfa
CatalinBarbu <121017880+CatalinBarbu@users.noreply.github.com>
2023-03-09 13:00:37 +0100

    add Product Card + Style

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
+import PlusBottomImage from "../../images/plusButton.png";
+
+import "./ProductCard.scss";
 const ProductCard = (props) => {
-    return ( 
-        <h2>productCard</h2>
-     );
-}
- 
+  return (
+    <section id="productCard">
+      <Link to={"/productDetails/" + props.product.id}>
+        <div>
+          <img className="productImg" src={props.product.images[0]} alt="" />
+        </div>
+        <div>
+          <p>
+            <span>⭐️</span>
+            {props.product.rating}
+          </p>
+          <article id="flexDetailsUndPreis">
+            <div>
+              <h1>{props.product.title}</h1>
+              <p>${props.product.price}</p>
+            </div>
+            <div className="productCardLastImg">
+              <img className="buttonImg" src={PlusBottomImage} alt="" />
+            </div>
+          </article>
+        </div>
+      </Link>
+    </section>
+  );
+};
+
 export default ProductCard;

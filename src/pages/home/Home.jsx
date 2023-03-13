326fad65ee2f756c430c65fb7f219eecbb484727
CatalinBarbu <121017880+CatalinBarbu@users.noreply.github.com>
2023-03-13 14:04:31 +0100

    handleSortByLowestPrice

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,27 +5,11 @@ import Footer from "../../components/footer/Footer";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
-import * as React from 'react';
 
 import "./Home.scss";
 
-
-const useLocalStorage = (storageKey, fallbackState) => {
-  const [value, setValue] = React.useState(
-    JSON.parse(window.sessionStorage.getItem(storageKey)) ?? fallbackState
-  );
-
-  React.useEffect(() => {
-    window.sessionStorage.setItem(storageKey, JSON.stringify(value));
-    
-    
-  }, [value, storageKey]);
-
-  return [value, setValue];
-};
-
 const Home = () => {
-  const [products, setProducts] = useLocalStorage("products", []);
+  let [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
   const [showDetailslist, setShowDetailslist] = useState(false);
   const [togglePopular, setTogglePopular] = useState(true);
@@ -39,25 +23,44 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
-    if (state) {
-      setProducts(state.products)
-      return
-    } 
-    if(products.length === 0){
-      fetch("https://dummyjson.com/products?limit=10")
+    fetch("https://dummyjson.com/products?limit=10")
       .then((res) => res.json())
       .then((products) => {
         console.log(products);
         return setProducts(products.products);
       })
       .catch((error) => console.error(error));
+  }, []);
 
-    }
-  }, [state]);
-
-
-    
-
+  function handleViewAllClick(e) {
+    e.preventDefault();
+    fetch("https://dummyjson.com/products?limit=100")
+      .then((res) => res.json())
+      .then((products) => setProducts(products.products))
+      .catch((error) => console.error(error));
+    document.querySelector("div .popular h3").innerHTML = "All Products";
+    document.querySelector("div .popular a").innerHTML = "Popular";
+    setTogglePopular(!togglePopular);
+    console.log(togglePopular);
+  }
+  function handlePopularClick(e) {
+    e.preventDefault();
+    fetch("https://dummyjson.com/products?limit=10")
+      .then((res) => res.json())
+      .then((products) => setProducts(products.products))
+      .catch((error) => console.error(error));
+    document.querySelector("div .popular h3").innerHTML = "Popular";
+    document.querySelector("div .popular a").innerHTML = "All Products";
+    setTogglePopular(!togglePopular);
+    console.log("test");
+  }
+  useEffect(() => {
+    if (state) return setProducts(state.products);
+    fetch("https://dummyjson.com/products?limit=10")
+      .then((res) => res.json())
+      .then((products) => setProducts(products.products))
+      .catch((error) => console.error(error));
+  }, []);
 
   function handleViewAllClick(e) {
     e.preventDefault();
@@ -82,6 +85,12 @@ const Home = () => {
     console.log("test");
   }
 
+  function handleSortByLowestPrice(e) {
+    e.preventDefault();
+    console.log(products);
+    products = products.sort((a, b) => a.price - b.price);
+    setProducts(products);
+  }
 
   return (
     <section id="home">
@@ -97,21 +106,31 @@ const Home = () => {
             return <Category category={category} setProducts={setProducts} />;
           })}
       </article>
-      <div className="popular">
-        <h3>Popular</h3>
-        <a
-          href="/"
-          onClick={(e) => {
-            if (togglePopular) {
-              handleViewAllClick(e);
-            } else {
-              handlePopularClick(e);
-            }
-          }}
-        >
-          All Products
-        </a>
-      </div>
+      {showDetailslist && (
+        <div>
+          <p>Sort by:</p>
+          <a href="" onClick={handleSortByLowestPrice}>
+            Lowest Price
+          </a>
+        </div>
+      )}
+      {!showDetailslist && (
+        <div className="popular">
+          <h3>Popular</h3>
+          <a
+            href="/"
+            onClick={(e) => {
+              if (togglePopular) {
+                handleViewAllClick(e);
+              } else {
+                handlePopularClick(e);
+              }
+            }}
+          >
+            All Products
+          </a>
+        </div>
+      )}
       <article className="allProducts">
         {products &&
           products.map((product) => {

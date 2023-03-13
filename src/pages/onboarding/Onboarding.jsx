21902d05ff4e9ee0bbfee18f26bbf44c9dc6e449
Lando Meyer <121017979+LandoADeveloper@users.noreply.github.com>
2023-03-13 13:40:34 +0100

    added initial fetch on onboarding

--- src/pages/onboarding/Onboarding.jsx
+++ src/pages/onboarding/Onboarding.jsx
@@ -3,14 +3,33 @@ import SplashScreen from "../../components/splashScreen/SplashScreen";
 import './Onboarding.scss';
 import illustration from '../../images/Illustration.png';
 
+import { useState, useEffect } from "react";
+
 const Onboarding = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+
+          fetch("https://dummyjson.com/products?limit=10")
+          .then((res) => res.json())
+          .then((products) => {
+            console.log(products);
+            return setProducts(products.products);
+          })
+          .catch((error) => console.error(error));
+    
+        
+      }, []);
+
+
     return (
         <section id="onboarding">
             <SplashScreen/>
             <img src={illustration} alt="" />
             <h1>Biggest Sell at Your Fingerprint</h1>
             <p>Find your best products from popular shop without any delay</p>
-            <Link to={"/home"}>Get Started</Link>
+            <Link to={"/home" } state= {{products:products}}>Get Started</Link>
         </section>
     );
 };
